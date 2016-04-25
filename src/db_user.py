d8d4b08267c9786b80d7f093f614a46dc2bb24db
yekeren <yekeren.cn@gmail.com>
2016-04-25 10:36:34 -0400

    files

--- src/db_user.py
+++ src/db_user.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import pickle
 import numpy as np
 
 import pdb
@@ -42,24 +43,32 @@ class UserDB(object):
                 items = line.split("\t")
                 user_id, user_data = json.loads(items[0]), json.loads(items[1])
 
-                self.db[user_id] = {
+                self.db[user_id] = UserInst({
                     "user_id"   : user_id,
                     "review_count"  : user_data[0],
                     "average_stars" : user_data[1],
                     "ratings"       : user_data[2],
                     "votes"         : user_data[3],
                     "reviews"       : user_data[4]
-                }
+                })
             print >> sys.stderr, "succ, %s records loaded" %(len(self.db))
 
+    def mark_history(self, file_name):
+        """ mark user history from file. """
+        with open(file_name, "r") as fp:
+            print >> sys.stderr, "loading history data from %s..." %(file_name)
+            user_dict = pickle.load(fp)
+            print >> sys.stderr, "succ, %s records loaded" %(len(userdict))
+
+            for user_id, user_inst in self.db.iteritems():
+                user_inst.mark_history(user_dict.get(user_id, dict()))
+            
     def __getitem__(self, user_id):
         """ get user instance by id. """
-        obj = self.db.get(user_id, None)
-        if obj is None:
-            return None
-        return UserInst(obj)
+        return self.db.get(user_id, None)
 
 if __name__ == "__main__":
-    user = UserDB("output/user.dat")
+    user_db = UserDB("output/user.dat")
+    user_db.mark_history("output/rating.train.0.history")
     a = user['V2IdhKBJXcl5t3cSZT6RNg']
-    print a.stars()
+    print a
--- src/partition.py
+++ src/partition.py
@@ -2,6 +2,7 @@
 
 import sys
 import json
+import pickle
 import numpy as np
 
 np.random.seed(477)
@@ -11,28 +12,34 @@ if __name__ == "__main__":
         print >> sys.stderr, "Usage: %s origin train_file test_file" %(sys.argv[0])
         exit(-1)
 
-    lines = []
+    records = []
     with open(sys.argv[1], "r") as fp:
         for line in fp.readlines():
             items = line.split("\t")
             user_id, user_data = json.loads(items[0]), json.loads(items[1])
             review_count, average_stars, ratings, votes, reviews = user_data
             for (business_id, stars, text) in reviews:
-                lines.append("%s\t%s\t%s\n" % (user_id, business_id, stars))
+                records.append((user_id, business_id, stars))
 
-    np.random.shuffle(lines)
+    np.random.shuffle(records)
 
     """ generate dataset for cross-validation. """
     batches_num = 10
-    batches_size = len(lines) / batches_num
+    batches_size = len(records) / batches_num
 
     for t in xrange(10):
+        userdict = dict()
         fp1 = open("%s.%s" %(sys.argv[2], t), "w")
         fp2 = open("%s.%s" %(sys.argv[3], t), "w")
-        for i, line in enumerate(lines):
+        for i, (user_id, business_id, stars) in enumerate(records):
+            line = "%s\t%s\t%s\n" %(user_id, business_id, stars)
             if i >= t * batches_size and i < (t + 1) * batches_size:
                 fp2.write(line)
             else:
+                userdict.setdefault(user_id, set()).add(business_id)
                 fp1.write(line)
         fp1.close()
         fp2.close()
+
+        with open("%s.%s.history" %(sys.argv[2], t), "wb") as fp:
+            pickle.dump(userdict, fp)
