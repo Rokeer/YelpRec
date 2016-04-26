7c287b97b53af28876c3b9f2c04a3c49c4d0c6f7
yekeren <yekeren.cn@gmail.com>
2016-04-26 17:05:32 -0400

    asdfsa

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -7,68 +7,25 @@ import numpy as np
 from util import *
 from sklearn import linear_model
 
-def load_users(file_name):
-    """ load users' feature. """
-    dct = dict()
-    with open(file_name, "r") as fp:
-        items = [ line.strip("\n").split("\t") for line in fp.readlines() ]
-        for item in items:
-            dct[item[0]] = (float(item[1]), float(item[2]), json.loads(item[3]))
-    print >> sys.stderr, "users loaded"
-    return dct
-
-def load_businesses(file_name):
-    """ load businesses' feature. """
-    dct = dict()
-    with open(file_name, "r") as fp:
-        items = [ line.strip("\n").split("\t") for line in fp.readlines() ]
-        for item in items:
-            dct[item[0]] = (float(item[1]), float(item[2]), json.loads(item[3]))
-    print >> sys.stderr, "businesses loaded"
-    return dct
-
-def load_data(file_name):
-    with open(file_name, "r") as fp:
-        items = [ line.strip("\n").split("\t") for line in fp.readlines() ]
-        items = map(lambda x: (x[0], x[1], float(x[2])), items)
-
-        vecs = []
-        y = []
-        for item in items:
-            u = users.get(item[0])
-            b = businesses.get(item[1])
-            if u and b:
-                u_star1, u_star2, u_lda = u
-                b_star1, b_star2, b_lda = b
-                vec = [u_star1, u_star2, b_star1, b_star2] + u_lda + b_lda
-                vecs.append(vec)
-                y.append(item[2])
-
-        x = np.zeros((len(vecs), len(vecs[0])))
-        for i, vec in enumerate(vecs):
-            x[i, :] = np.array(vec)
-        y = np.array(y)
-    return x, y
-
 if __name__ == "__main__":
-    users = load_users("output/user.lda")
-    businesses = load_businesses("output/business.lda")
-
     mse = np.zeros((10, 1))
     rmse = np.zeros((10, 1))
     r_squared = np.zeros((10, 1))
     precision = np.zeros((10, 1))
 
     for t in xrange(10):
-        x_train, y_train = load_data("output/rating.train.%s" %(t))
-        x_test, y_test = load_data("output/rating.test.%s" %(t))
+        file_object = np.load("output/raw_feature.%s" %(t))
+        x_train = file_object["x_train"]
+        y_train = file_object["y_train"]
+        x_test = file_object["x_test"]
+        y_test = file_object["y_test"]
 
         regr = linear_model.LinearRegression()
         regr.fit(x_train, y_train)
 
         y_pred = regr.predict(x_test)
         mse[t], rmse[t], r_squared[t], precision[t] = evaluate(y_test, y_pred)
-        print >> sys.stderr, "cross validation", t, "done"
+        print >> sys.stderr, "cross validation %s done, r_squared=%.4lf, precision=%.4lf" %(t, r_squared[t], precision[t])
 
     print "mse:       %.4lf"  % (mse.mean())
     print "rmse:      %.4lf"  % (rmse.mean())
